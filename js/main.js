d2a207db5a0ff286a3b0179179040343b99e635b
Mohammed Seyam <m05921033@gmail.com>
2021-10-20 17:16:13 +0300

    my first commit

---
+++ js/main.js
@@ -0,0 +1,149 @@
+
+// function convertToShakel(){
+//     var usdcoins = document.getElementById("usdcoin").value ;
+//     var result = usdcoins * 3.2 ;
+//     if(usdcoins === "" || usdcoins <= 0 ){
+//         alert("not accept number");
+//     }
+//     else{
+//         document.getElementById("show").innerHTML = result;
+//         console.log(result);
+//     }
+  
+  
+// }
+// var fruitName = prompt("please enter your favorite fruit");
+// switch( fruitName ){
+//     case "mango" : alert("this fruit is yummy yummy") ; 
+//     break;
+//     case "apple" : alert("apple contian a lot of protine") ;
+//     break;
+//     case "banana" : ;
+//     case "orange" : alert("contains a lot of vitamin c") ;
+//     break ;
+//     default : alert("please enter a fruit name") ; 
+    
+
+// }
+var employes = ["mohammed" , "ahmed"  , "hazem" , "alaa" ];
+var newnumber = [];
+// console.log(employes);
+// console.log("//////////////////////")
+// employes.unshift("khaled");
+// console.log(employes);
+// employes.shift();
+// console.log(employes);
+
+// employes[1]= "soso";
+// employes.pop();
+// console.log(employes);
+// console.log(employes[0]);
+// console.log(employes[1]);
+// console.log(employes[2]);
+// console.log(employes[3]);
+// console.log(employes[4]);
+// for(var i = 0 ; i > employes.length ; i++){
+//     console.log(employes[i]); 
+
+// }
+// var i =0 ;
+// while(i < employes.length){
+//     console.log(employes[i]); 
+    
+//     i++
+
+// }
+var i =0;
+do{
+    console.log(employes[i]); 
+    i++;
+}while(i > employes.length)
+
+
+
+
+
+
+
+
+
+
+// var itemPrice = 250,
+// Nitems = 7;
+
+// if(itemPrice >= 200 && Nitems >=5 ){
+//     alert("you have a discount 10%");
+// }else {
+//     alert("no discount");
+// }
+
+// var pass = "1231233333";
+// // console.log(pass);
+
+// if( pass != "123123"){
+// alert("Done Welcom");
+// }else{
+//     alert("enter the correct password");
+// }
+// var age = 25;
+
+// if(age > 16){
+//     alert("Welcom");
+// }else{
+//     alert("you are under age")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("this is a alert")
+// document.write("this is a test text")
+
+
+// document.getElementById("Log").innerHTML = "hi mohammed    " ;
+// var theName1 = 'Ali';
+// console.log(theName1)
+
+// var x = 30,
+//     y = 10,
+//     name = "mohamemd",
+//     result = x + y ;
+    
+//  console.log(typeof [1,5,6,8]);   
+
+
+// console.log("your name is : \n" + name );
+// console.log(age);
+// console.log(name);
+// console.log("hi \"Ali\" ");
+
+
+
+
+
+
